941333acdbf7e48406705d92a398a9101d173c28
lulu <lulu@muchenglin.com>
2021-08-24 19:54:12 +0800

    first commit

---
+++ src/main/java/com/lucy/aitestdemo/AitestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lucy.aitestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AitestDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AitestDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucy/aitestdemo/AitestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucy.aitestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AitestDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
